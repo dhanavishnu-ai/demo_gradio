dfb56fcbef78a5079d678e68adcf869232778b6d
DHANAVISHNU A <vishnukvka@gmail.com>
2023-09-12 13:25:24 +0530

    updated

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import gradio as gr
 def greet(name):
     return "Hello " + name + "!"
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+# demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+
+with gr.Blocks() as demo:
+    gr.Markdown('# <center>INTELLIGENT ANALYSIS OF WEBSITE DRIVEN QA CHATBOT</center>')
     
 if __name__ == "__main__":
     demo.queue().launch(debug = True,server_name="0.0.0.0", server_port=8000)
